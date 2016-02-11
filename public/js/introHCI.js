71d7551826db76f1a9029b7abf93df34661e83d5
hungale <zenithparadox@gmail.com>
2016-02-11 20:23:48 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,37 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	console.log("GETTING /project/" + idNumber);
+	$.get("/project/" + idNumber, addProject);
+}
+
+function addProject(result) {
+	// result is a JSON with fields id, title, date, summary
+	
+	// grabs the div with class project, id {{id}}
+	//$('#project' + result['id'] + ' .details').html("please work");
+
+	console.log(result['image']);
+	var projectHTML = '<img src="' + result['image'] + '"class="detailsImage">'+
+		'<h4>' + result['title'] + '</h4>' +
+		'<p>' + result['date'] + '</p>' +
+		'<p>' + result['summary'] + '</p>';
+	$('#project' + result['id'] + ' .details').html(projectHTML);	
+}
+
+function changeColors (result) { 
+	//call back fxn w/ color information
+
+	// grabs the hex array within colors
+	var colors = result['colors']['hex'];
+
+	// edit the css
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
 
 /*
@@ -35,4 +66,7 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	
+	// call the callback fxn
+  $.get("/palette", changeColors);
 }
